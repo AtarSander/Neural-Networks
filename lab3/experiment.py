1a788b53f530fea3499da9d0325f48f25fb3221d
ola <asz-02@wp.pl>
2025-04-12 01:20:41 +0200

    after training best model for 30 epochs

--- lab3/experiment.py
+++ lab3/experiment.py
@@ -12,6 +12,7 @@ from model import Model, train, predict
 from torch.utils.data import DataLoader
 import torch
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 
 def tune_architecture(
@@ -78,30 +79,30 @@ def tune_architecture(
         val_confusion_matrix.append(confusion_matrix(val_labels, val_y_preds))
         print(classification_report(val_labels, val_y_preds))
 
-        with torch.no_grad():
-            logits = train_y_preds
-            print(logits, logits.shape)
-            probs = (
-                F.softmax(torch.from_numpy(logits), dim=1).cpu().numpy()
-            )  # i know np.darray->tensor->np.darray is ugly, but dont want to use scipy just for softmax
-            n_classes = probs.shape[1]
-            roc_data = []
-            for i in range(n_classes):
-                fpr, tpr, _ = roc_curve(train_labels == i, probs[:, i])
-                roc_auc = auc(fpr, tpr)
-                roc_data.append((fpr, tpr, roc_auc))
-            train_roc_curve.append(roc_data)
+        # with torch.no_grad():
+        #     logits = train_y_preds
+        #     print(logits, logits.shape)
+        #     probs = (
+        #         F.softmax(torch.from_numpy(logits), dim=1).cpu().numpy()
+        #     )  # i know np.darray->tensor->np.darray is ugly, but dont want to use scipy just for softmax
+        #     n_classes = probs.shape[1]
+        #     roc_data = []
+        #     for i in range(n_classes):
+        #         fpr, tpr, _ = roc_curve(train_labels == i, probs[:, i])
+        #         roc_auc = auc(fpr, tpr)
+        #         roc_data.append((fpr, tpr, roc_auc))
+        #     train_roc_curve.append(roc_data)
 
-        with torch.no_grad():
-            logits = val_y_preds
-            probs = F.softmax(torch.from_numpy(logits), dim=1).cpu().numpy()
-            n_classes = probs.shape[1]
-            roc_data = []
-            for i in range(n_classes):
-                fpr, tpr, _ = roc_curve(val_labels == i, probs[:, i])
-                roc_auc = auc(fpr, tpr)
-                roc_data.append((fpr, tpr, roc_auc))
-            val_roc_curve.append(roc_data)
+        # with torch.no_grad():
+        #     logits = val_y_preds
+        #     probs = F.softmax(torch.from_numpy(logits), dim=1).cpu().numpy()
+        #     n_classes = probs.shape[1]
+        #     roc_data = []
+        #     for i in range(n_classes):
+        #         fpr, tpr, _ = roc_curve(val_labels == i, probs[:, i])
+        #         roc_auc = auc(fpr, tpr)
+        #         roc_data.append((fpr, tpr, roc_auc))
+        #     val_roc_curve.append(roc_data)
 
     return (
         losses,
@@ -119,9 +120,15 @@ def tune_architecture(
         val_roc_curve,
     )
 
-
-def tune_hyperparameters(
-    train_dataset, val_dataset, architecture, hyperparameters, device, verbose=False
+def resume_tuning(
+    model_path,
+    train_dataset,
+    val_dataset,
+    architecture,
+    training_setup,
+    device,
+    verbose=False,
+    save_weights=False,
 ):
     losses = []
     train_accuracy = []
@@ -137,75 +144,48 @@ def tune_hyperparameters(
     train_roc_curve = []
     val_roc_curve = []
 
-    train_labels = [label for _, label in train_dataset]
-    train_images = [image for image, _ in train_dataset]
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=training_setup["batch_size"], shuffle=True
+    )
+    val_dataloader = DataLoader(
+        val_dataset, batch_size=training_setup["batch_size"], shuffle=False
+    )
 
-    val_labels = [label for _, label in val_dataset]
-    val_images = [image for image, _ in val_dataset]
+    model = Model(architecture, device)
+    model.load_weights(model_path)
+    print(f"Training model")
+    loss = train(
+        model,
+        train_dataloader,
+        training_setup["epochs"],
+        training_setup["lr"],
+        training_setup["batch_size"],
+        training_setup["class_weights"],
+        verbose=verbose,
+        save_checkpoints=save_weights,
+        identifier=f"t_ar_resume",
+    )
+    print("Training done")
+    model.eval()
+    losses.append(loss)
+    train_y_preds, train_labels = predict(model, train_dataloader)
+    train_accuracy.append(accuracy_score(train_labels, train_y_preds))
+    train_precision.append(
+        precision_score(train_labels, train_y_preds, average="macro")
+    )
+    train_recall.append(recall_score(train_labels, train_y_preds, average="macro"))
+    train_f1.append(f1_score(train_labels, train_y_preds, average="macro"))
+    train_confusion_matrix.append(confusion_matrix(train_labels, train_y_preds))
 
-    best_model = None
-    best_model_recall = 0
-
-    for i, hyperparameter in enumerate(hyperparameters):
-        model = Model(architecture, device)
-        print(f"Training model {i}")
-        loss = train(
-            model,
-            train_dataset,
-            hyperparameter["epochs"],
-            hyperparameter["lr"],
-            hyperparameter["batch_size"],
-            hyperparameter["class_weights"],
-            verbose=verbose,
-        )
-        print("Training done")
-        losses.append(loss)
-        train_y_preds = predict(model, train_images)
-        train_accuracy.append(accuracy_score(train_labels, train_y_preds))
-        train_precision.append(
-            precision_score(train_labels, train_y_preds, average="macro")
-        )
-        train_recall.append(recall_score(train_labels, train_y_preds, average="macro"))
-        train_f1.append(f1_score(train_labels, train_y_preds, average="macro"))
-        train_confusion_matrix.append(confusion_matrix(train_labels, train_y_preds))
-
-        val_y_preds = predict(model, val_images)
-        val_accuracy.append(accuracy_score(val_labels, val_y_preds))
-        val_precision.append(precision_score(val_labels, val_y_preds, average="macro"))
-        val_recall.append(recall_score(val_labels, val_y_preds, average="macro"))
-        val_f1.append(f1_score(val_labels, val_y_preds, average="macro"))
-        val_confusion_matrix.append(confusion_matrix(val_labels, val_y_preds))
-        print(classification_report(val_labels, val_y_preds))
-
-        if val_recall[-1] > best_model_recall:
-            best_model = model
-            best_model_recall = val_recall[-1]
-
-        model.eval()
-        with torch.no_grad():
-            logits = model(torch.tensor(train_images).float().to(device))
-            probs = F.softmax(logits, dim=1).cpu().numpy()
-            n_classes = probs.shape[1]
-            roc_data = []
-            for i in range(n_classes):
-                fpr, tpr, _ = roc_curve(train_labels == i, probs[:, i])
-                roc_auc = auc(fpr, tpr)
-                roc_data.append((fpr, tpr, roc_auc))
-            train_roc_curve.append(roc_data)
-
-        with torch.no_grad():
-            logits = model(torch.tensor(val_images).float().to(device))
-            probs = F.softmax(logits, dim=1).cpu().numpy()
-            n_classes = probs.shape[1]
-            roc_data = []
-            for i in range(n_classes):
-                fpr, tpr, _ = roc_curve(val_labels == i, probs[:, i])
-                roc_auc = auc(fpr, tpr)
-                roc_data.append((fpr, tpr, roc_auc))
-            val_roc_curve.append(roc_data)
+    val_y_preds, val_labels = predict(model, val_dataloader)
+    val_accuracy.append(accuracy_score(val_labels, val_y_preds))
+    val_precision.append(precision_score(val_labels, val_y_preds, average="macro"))
+    val_recall.append(recall_score(val_labels, val_y_preds, average="macro"))
+    val_f1.append(f1_score(val_labels, val_y_preds, average="macro"))
+    val_confusion_matrix.append(confusion_matrix(val_labels, val_y_preds))
+    print(classification_report(val_labels, val_y_preds))
 
     return (
-        best_model,
         losses,
         train_accuracy,
         train_precision,
@@ -220,3 +200,12 @@ def tune_hyperparameters(
         val_confusion_matrix,
         val_roc_curve,
     )
+
+def plot_losses(losses):
+    for index in range(len(losses)):
+        plt.plot(losses[index][0], losses[index][1], label="loss")
+        plt.xlabel("epoch")
+        plt.ylabel("loss")
+        plt.title(f"Loss vs Epoch")
+        plt.legend()
+        plt.show()
